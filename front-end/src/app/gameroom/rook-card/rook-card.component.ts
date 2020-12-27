8b0abb6be274f73dd5909c56255fc2e49cd2b457
gkillick <gkillick@gmail.com>
2020-12-26 21:31:30 -0700

    flipping state for cards

--- front-end/src/app/gameroom/rook-card/rook-card.component.ts
+++ front-end/src/app/gameroom/rook-card/rook-card.component.ts
@@ -1,10 +1,12 @@
+import { animate, state, style, transition, trigger } from '@angular/animations';
 import { Component, Input, OnInit } from '@angular/core';
 import { Card } from '../card.model';
 
 @Component({
   selector: 'app-rook-card',
   templateUrl: './rook-card.component.html',
-  styleUrls: ['./rook-card.component.css']
+  styleUrls: ['./rook-card.component.css'],
+  
 })
 export class RookCardComponent implements OnInit {
   @Input() card: Card;
@@ -12,6 +14,14 @@ export class RookCardComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  cardClicked() {
+    if (this.card.state === "face") {
+      this.card.state = "flipped";
+    } else {
+      this.card.state = "face";
+    }
   }
 
 }
