4367ff103b15da1bf8997092c6a243cf164bf720
Micah Killick <killickster@gmail.com>
2020-12-30 23:13:20 -0700

    added bidding logic in socket

--- server/src/routes/sockets/game_socket/game_socket.ts
+++ server/src/routes/sockets/game_socket/game_socket.ts
@@ -104,6 +104,14 @@ module.exports = function(io: any){
                         return io.of('/games/socket').to(game_id).emit('game_state_changed', {game_id: game_id})
                     })
 
+                }else if(play.moveType === MoveType.BID){
+                   
+                    var play = new Play(play.moveType, play.player_id, play.payload)
+
+                    game.move(play).then((index: any) => {
+
+                        return io.of('/games/socket').to(game_id).emit('game_state_changed', {game_id: game_id})
+                    })
                 }
 
 
