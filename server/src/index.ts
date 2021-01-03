0a980828d6eefb79a50240339949e8bdf12cbc09
Micah Killick <killickster@gmail.com>
2021-01-03 14:39:25 -0700

    did something

--- server/src/index.ts
+++ server/src/index.ts
@@ -29,12 +29,13 @@ app.use('/api/user', authRoute)
 app.use('/api/posts', postRoute)
 app.use('/api/games', gameRoute)
 
-server.listen(3000, 'micah.local', () => {console.log("Server is up and running")})
+server.listen(3000, process.env.SERVER , () => {console.log("Server is up and running")})
 
+var url = 'http://' + process.env.SERVER + ":4200"
 
 const io = require('socket.io')(server, {
     cors: {
-        origin:'http://micah.local:4200',
+        origin: url,
         method:["GET", "POST"]
     }
 })
